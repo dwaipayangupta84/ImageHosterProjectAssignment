571088c39c75278137cf96d4a4587daf2a614ca3
dwgupta <dwgupta@DWGUPTA-AE.ae.oracle.com>
2020-10-26 21:49:49 +0400

    1st Milestome - Logic Incorporated for search by ImageID

--- src/main/java/ImageHoster/config/JpaConfig.java
+++ src/main/java/ImageHoster/config/JpaConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
